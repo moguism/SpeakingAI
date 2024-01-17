0eeef799fdf783628d633113616aa2277b95195a
moguism <rosso.aboy@digitechfp.com>
2024-01-17 15:13:26 +0100

    Agregado TTS con Google

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import json
 from vosk import Model, KaldiRecognizer
 import wave
+from gtts import gTTS
+from playsound import playsound
 
 def transcribir_audio(ruta_audio):
-    modelo_vosk = Model("vosk-model-small-en-us-0.15")  # Reemplaza con la ruta al modelo Vosk
+    modelo_vosk = Model("vosk-model-small-en-us-0.15")
     reconocedor = KaldiRecognizer(modelo_vosk, 16000)
 
     with wave.open(ruta_audio, 'rb') as audio_file:
@@ -16,6 +18,11 @@ def transcribir_audio(ruta_audio):
 
     return transcripcion_texto
 
+def reproducir_tts(texto):
+    tts = gTTS(text=texto, lang='en')
+    tts.save("transcripcion.mp3")
+    playsound("transcripcion.mp3")
+
 if __name__ == "__main__":
     archivo_audio = "grabacion.wav"
 
@@ -23,5 +30,7 @@ if __name__ == "__main__":
         transcripcion = transcribir_audio(archivo_audio)
         print("Transcripción del audio:")
         print(transcripcion)
+
+        reproducir_tts(transcripcion)
     else:
         print(f"El archivo {archivo_audio} no existe.")
